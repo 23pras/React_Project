75e20b09f40084f332322692c067f6709df9b55d
23pras <palprashant465@gmail.com>
2022-04-18 15:13:01 +0530

    Theme button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
 //import logo from "./logo.svg";
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" about="About Text" />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" />
+        {/* <TextForm heading="Enter the text to analyze below" /> */}
+        <About/>
       </div>
     </>
   );
